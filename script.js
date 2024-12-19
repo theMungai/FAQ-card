436bbaccdc8a071204d6966062dc9e9234194eb7
theMungai <mungaithesimon@gmail.com>
2024-12-19 16:25:35 +0300

    Media Queries for all devices

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ const questions = document.querySelectorAll(".questions-answers-container");
 
 questions.forEach((question) => {
     const answer = question.querySelector(".answer");
-    const arrow = question.querySelector(".arrow-down")
+    const arrow = question.querySelector(".arrow-down");
+    const questionHeader = question.querySelector(".question")
     question.addEventListener("click", () => {
         const isOpen = answer.style.display === "block";
 
         // reset all questions and arrows
         answer.style.display = "none";
-        arrow.classList.remove("arrow-up")
+        arrow.classList.remove("arrow-up");
+        question.classList.remove("question-active");
 
         if(isOpen){
             answer.style.display = "none"
             arrow.classList.remove("arrow-up");
+            questionHeader.classList.remove("question-active");
         }
         else{
             answer.style.display = "block";
             arrow.classList.add("arrow-up");
+            questionHeader.classList.add("question-active");
         }
     })
 });
